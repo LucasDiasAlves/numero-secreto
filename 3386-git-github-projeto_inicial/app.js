0045f552e26cfa50b26ce6ec3522d529490cc1d6
LucasDiasAlves <fizzm5197@gmail.com>
2025-01-21 17:10:45 -0300

    git push

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,10 +1,9 @@
 let listaDeNumerosSorteados = [];
-
-
-
 let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
+console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
